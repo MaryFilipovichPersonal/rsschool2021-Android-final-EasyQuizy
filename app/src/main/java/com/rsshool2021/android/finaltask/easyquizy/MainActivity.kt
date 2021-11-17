361f39bc8348889bcf5f8d394d777b45caea7557
MaryFilipovichPersonal <maria0filipovich@gmail.com>
2021-11-17 13:20:38 +0300

    init: Initial commit

---
+++ app/src/main/java/com/rsshool2021/android/finaltask/easyquizy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rsshool2021.android.finaltask.easyquizy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
